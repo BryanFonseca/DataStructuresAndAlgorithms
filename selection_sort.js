5c5b4b5fe5180247ee971b721db6c5a736084ffd
Bryan Fonseca <isaac1805@hotmail.com>
2021-07-03 20:40:09 -0500

    Add cleaner version of Selection Sort

--- selection_sort.js
+++ selection_sort.js
@@ -2,33 +2,57 @@ let arreglo = [9, 8, 4, 3, 2, 1, 3, 6, 8, 5, 4, 7, 6, 5, 4, 2, 3, 1]
 
 function selection_sort(list)
 {
-    let sorted = 0;
-    let min_sorted_index = 0;
-    
-    while(!sorted)
+  let sorted = 0;
+  let min_sorted_index = 0;
+
+  while(!sorted)
+  {
+    let index_less_so_far = min_sorted_index;
+    for(let i = min_sorted_index; i < list.length; i++)
     {
-        let index_less_so_far = min_sorted_index;
-        for(let i = min_sorted_index; i < list.length; i++)
-        {
-            if(list[i + 1] < list[index_less_so_far])
-            {
-                index_less_so_far = i + 1;
-            }
-        }
-        if(min_sorted_index != index_less_so_far)
-        {
-            let swap_helper = list[min_sorted_index];
-            list[min_sorted_index] = list[index_less_so_far];
-            list[index_less_so_far] = swap_helper;
-        }        
-        if((list.length - 1) - (min_sorted_index - 1) == 1)
-        {
-            if(list[min_sorted_index - 1] < list[list.length - 1])
-                sorted = true;
-        }
-        min_sorted_index++;
+      if(list[i + 1] < list[index_less_so_far])
+      {
+	index_less_so_far = i + 1;
+      }
     }
-    return list;
+    if(min_sorted_index != index_less_so_far)
+    {
+      let swap_helper = list[min_sorted_index];
+      list[min_sorted_index] = list[index_less_so_far];
+      list[index_less_so_far] = swap_helper;
+    }        
+    if((list.length - 1) - (min_sorted_index - 1) == 1)
+    {
+      if(list[min_sorted_index - 1] < list[list.length - 1])
+	sorted = true;
+    }
+    min_sorted_index++;
+  }
+  return list;
 }
 
 console.log(selection_sort(arreglo));
+
+function selection_sort_book(list)
+{
+  for(let i = 0; i < list.length - 1; i++)
+  {
+    let lowestNumberIndex = i;
+    for(let j = i + 1; j < list.length; j++)
+    {
+      if(list[j] < list[lowestNumberIndex])
+      {
+	lowestNumberIndex = j;
+      }
+    }
+    if(lowestNumberIndex != i)
+    {
+      let temp = list[i];
+      list[i] = list[lowestNumberIndex];
+      list[lowestNumberIndex] = temp;
+    }
+  }
+  return list;
+}
+
+console.log(selection_sort_book(arreglo));
